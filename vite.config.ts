0146579104455b84a0d0893a60519f76f0f700e8
sebashtioon <sebastiansuciu607@gmail.com>
2025-08-18 06:37:06 +1000

    nice

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { cloudflare } from "@cloudflare/vite-plugin"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,12 +10,8 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    cloudflare({
-      // Cloudflare specific options
-    }),
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
