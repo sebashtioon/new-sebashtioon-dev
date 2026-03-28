9243a145dae001acf1dccc8c93f432aeff807d7c
sebashtioon <sebastiansuciu607@gmail.com>
2026-03-28 14:49:57 +1100

    yes

--- src/content/blogPosts.tsx
+++ src/content/blogPosts.tsx
@@ -16,28 +16,72 @@ export const blogPosts: BlogPost[] = [
     summary: "an endless yap about my latest game",
     content: (
       <>
-        <p className="lowercase">
-          okay so... what actually <em>is</em> this game???
-        </p>
+        <p className="lowercase">okay so... what actually is this game???</p>
 
         <p className="lowercase">
-          one day a few years ago i was scrolling yt shorts and saw a video about sleep paralysis. i hadnt known what it was about prior to this one video. i did a lil research on it and it seemed pretty cool to me...
+          one day a few years ago i was scrolling yt shorts (
+          <img
+            src="/emojis/heavysob.png"
+            alt="heavysob"
+            className="inline-block h-5 w-5 align-text-bottom"
+            loading="lazy"
+          />
+          ) and saw a video about sleep paralysis. i hadnt known what it was about prior to this one video. i did a lil research on it and it seemed pretty cool to me...
         </p>
 
         <p className="lowercase">until i had my own sleep paralysis experience.</p>
 
-        <p className="font-bold normal-case">AT ALL</p>
+        <p className="lowercase">that was not fun.</p>
+
+        <p className="font-bold normal-case">
+          AT ALL{" "}
+          <img
+            src="/emojis/3kcursed.png"
+            alt="3kcursed"
+            className="inline-block h-5 w-5 align-text-bottom"
+            loading="lazy"
+          />
+        </p>
 
         <p className="lowercase">
           in fact, this game is mostly based off of that one experience. (not the backstory or lore that i will get into later)
         </p>
 
         <p className="lowercase">
-          okay okay well if you dont know what sleep paralysis is, its pretty much your mind being awake but your body sound asleep. and if you ask me this is quite fucking stupid like why has evolution not solved this problem gng :skulk: and for some reason, your eyes arent paralyzed like the rest of your body is so you know what that means. <span className="font-bold normal-case">YOU OPEN THEM!!!</span> oh, but because youre also "dreaming" at the same time as being paralyzed that means your brain goes haywire and creates things that arent real (i.e. hallucinations) these feel ultra real because your brain is mixing dream shit with actual reality (e.g. <strong>backrooms entity looking ahh</strong> and <strong>your bedroom</strong>). on top of that, your brain is so evolved that you can actually <em>FEEL</em> things that happen in this dream/reality hybrid. so that means, if the demon or figure (whatever fuckery your noodle is brewing up this week) is on your bed with you, you can <strong><em>feel</em></strong> it. this might be a weight on your chest or breathing in your face. but dont worry, these demons or dream things happening in your room <em>arent actually real.</em> well, thats not quite the case for this game (mostly).
+          okay okay well if you dont know what sleep paralysis is, its pretty much your mind being awake but your body sound asleep. and if you ask me this is quite fucking stupid like why has evolution not solved this problem gng :skulk: and for some reason, your eyes arent paralyzed like the rest of your body is so you know what that means{" "}
+          <img
+            src="/emojis/thumbup.jpg"
+            alt="thumbup"
+            className="inline-block h-5 w-5 align-text-bottom"
+            loading="lazy"
+          />
         </p>
 
         <p className="lowercase">
-          this game is called <span className="normal-case">"CANTWAKEUP"</span>. yes, in all caps so i make it seem super scary. i might refer to it as "cwu" from here on out just because its less effort to type out and also cuz im lazy. after this sleep paralysis episode of mine, i suddenly just had an idea. to make a game out of it. i was planning this game to be playable at the same length that would go in a markiplier video or playthrough (your typical psx/ps1 style short horror game thats around 30min long). this was only thought up relatively recently, but the backstory of the game is, youre a kid (probably like 9-10 years old, so quite young) and you just moved into a new house. and guess what? you have severe sleep paralysis episodes!!!! your parents are out at work while youre left to suffer alone at home. they leave you tasks/chores to do on a peice of paper on the kitchen table everyday, and you need to complete them or your sleep paralysis episodes get worse. i mean regardless of whether you complete them or not youre gonna have <em>something</em> that night but it wont be as harsh as it would be if you didnt do any of the tasks.
+          YOU OPEN THEM!!! oh, but because youre also "dreaming" at the same time as being paralyzed that means your brain goes haywire and creates things that arent real (i.e. hallucinations) these feel ultra real because your brain is mixing dream shit with actual reality (e.g. backrooms entity looking ahh and your bedroom). on top of that, your brain is so evolved that you can actually FEEL things that happen in this dream/reality hybrid. so that means, if the demon or figure (whatever fuckery your noodle is brewing up this week) is on your bed with you, you can feel it.
+        </p>
+
+        <p className="lowercase">
+          this might be a weight on your chest or breathing in your face. but dont worry, these demons or dream things happening in your room arent actually real. well, thats not quite the case for this game (mostly).
+        </p>
+
+        <p className="lowercase">
+          this game is called "CAN'T WAKE UP". yes, in all caps so i make it seem super scary
+          <img
+            src="/emojis/shocked.png"
+            alt="shocked"
+            className="inline-block h-5 w-5 align-text-bottom"
+            loading="lazy"
+          />
+          . i might refer to it as "cwu" from here on out just because its less effort to type out and also cuz im lazy. after this sleep paralysis episode of mine, i suddenly just had an idea. to make a game out of it.
+        </p>
+
+        <p className="lowercase">
+          i was planning this game to be playable at the same length that would go in a markiplier video or playthrough (your typical psx/ps1 style short horror game thats around 30min long). this was only thought up relatively recently, but the backstory of the game is, youre a kid (probably like 9-10 years old, so quite young) and you just moved into a new house. and guess what? you have severe sleep paralysis episodes !! :yayayayayay:
+        </p>
+
+        <p className="lowercase">
+          your parents are out at work while youre left to suffer alone at home. they leave you tasks/chores to do on a peice of paper on the kitchen table everyday, and you need to complete them or your sleep paralysis episodes get worse. i mean regardless of whether you complete them or not youre gonna have something that night but it wont be as harsh as it would be if you didnt do any of the tasks.
         </p>
 
         <p className="lowercase">
@@ -45,11 +89,51 @@ export const blogPosts: BlogPost[] = [
         </p>
 
         <ul className="list-disc pl-6 lowercase text-foreground/90">
-          <li>the pizza in the fridge being green for some reason</li>
-          <li>bloody hand prints on the mirror after shower</li>
-          <li>tv playing weird stuff/distortion</li>
-          <li>when folding clothes, one shirt is covered in blood/has a bloody handprint on it</li>
-          <li>doing a task but it actually "undoes" itself so you have to do it again</li>
+          <li>
+            the pizza in the fridge being green for some reason{" "}
+            <img
+              src="/emojis/3kcursed.png"
+              alt="3kcursed"
+              className="inline-block h-5 w-5 align-text-bottom"
+              loading="lazy"
+            />
+          </li>
+          <li>
+            bloody hand prints on the mirror after shower{" "}
+            <img
+              src="/emojis/3kcursed.png"
+              alt="3kcursed"
+              className="inline-block h-5 w-5 align-text-bottom"
+              loading="lazy"
+            />
+          </li>
+          <li>
+            tv playing weird stuff/distortion{" "}
+            <img
+              src="/emojis/3kcursed.png"
+              alt="3kcursed"
+              className="inline-block h-5 w-5 align-text-bottom"
+              loading="lazy"
+            />
+          </li>
+          <li>
+            when folding clothes, one shirt is covered in blood/has a bloody handprint on it{" "}
+            <img
+              src="/emojis/3kcursed.png"
+              alt="3kcursed"
+              className="inline-block h-5 w-5 align-text-bottom"
+              loading="lazy"
+            />
+          </li>
+          <li>
+            doing a task but it actually "undoes" itself so you have to do it again{" "}
+            <img
+              src="/emojis/3kcursed.png"
+              alt="3kcursed"
+              className="inline-block h-5 w-5 align-text-bottom"
+              loading="lazy"
+            />
+          </li>
         </ul>
 
         <p className="lowercase">
@@ -61,7 +145,13 @@ export const blogPosts: BlogPost[] = [
         </p>
 
         <p className="lowercase">
-          what would be a sleep paralysis episode without a demon? i was brainstorming ideas with my friend once and he said i should name him kai. so, say hi to kai!
+          what would be a sleep paralysis episode without a demon? i was brainstorming ideas with my friend once and he said i should name him kai. so, say hi to kai!{" "}
+          <img
+            src="/emojis/hii.gif"
+            alt="hii"
+            className="inline-block h-5 w-5 align-text-bottom"
+            loading="lazy"
+          />
         </p>
 
         <img
@@ -72,7 +162,13 @@ export const blogPosts: BlogPost[] = [
         />
 
         <p className="lowercase">
-          i was super excited to start making it so i got straight to it, even though i didnt have a general idea of the lore or story in the game (let alone what the player actually does) back then. i started making a simple layout of a house, and just the general room setup and stuff, as seen below. yes, this was done in canva
+          i was super excited to start making it so i got straight to it, even though i didnt have a general idea of the lore or story in the game (let alone what the player actually does) back then. i started making a simple layout of a house, and just the general room setup and stuff, as seen below. yes, this was done in canva{" "}
+          <img
+            src="/emojis/heavysob.png"
+            alt="heavysob"
+            className="inline-block h-5 w-5 align-text-bottom"
+            loading="lazy"
+          />
         </p>
 
         <img
