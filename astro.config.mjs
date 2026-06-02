377d97eabf15cf36680dc90c5aa8519051b47a81
seb <sebastiansuciu607@gmail.com>
2026-06-02 00:13:07 +0000

    wefwepofjoijew

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -7,5 +7,10 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Arial",
+    cssVariable: "--font-arial",
+  }]
 });
