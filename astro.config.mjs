4a1a1629586f50bacda69eec94b4794c3235be4b
sebashtioon <sebastiansuciu607@gmail.com>
2026-06-18 23:25:03 +1000

    yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,13 +11,9 @@ export default defineConfig({
   fonts: [
     {
       provider: fontProviders.fontsource(),
-      name: "Arial",
-      cssVariable: "--font-arial",
-    },
-    {
-      provider: fontProviders.fontsource(),
       name: "Inter",
-      cssVariable: "--font-inter",
+      cssVariable: "--font-inter-asset",
+      styles: ["normal"],
     },
   ],
 });
